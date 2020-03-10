8a11eb5794cced5e985756c921c5cd8ff889aaf1
Peter Charalambous <peter.charalambous@anz.com>
2020-03-10 12:29:08 +1100

    Added test

---
+++ cypress/integration/test/test.js
@@ -0,0 +1,21 @@
+describe('Nav site', () => {
+    it('Should nav to home', () => {
+        cy.visit('/');
+        cy.get('h1').contains('Homepage');
+    })
+
+    it('Should nav to about', () => {
+        cy.get('a').contains('About').click();
+        cy.get('h1').contains('About');
+    })
+
+    it('Should nav to contact', () => {
+        cy.get('a').contains('Contact').click();
+        cy.get('h1').contains('Contact');
+    })
+
+    it('Should fail on purpose', () => {
+        cy.get('a').contains('Home').click();
+        cy.get('h1').contains('NotHome');
+    })
+})
